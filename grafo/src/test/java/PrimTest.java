432a3d4dda5e203236dbc51bb7cb7f48a4a862fd
JoacoPrida <163531115+JoacoPrida@users.noreply.github.com>
2025-07-23 11:34:41 -0300

    Make gradle executable and update tests

--- grafo/src/test/java/PrimTest.java
+++ grafo/src/test/java/PrimTest.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PrimTest {
     @Test
     public void testRun() {
         EdgeWeightedIntGraph g = new EdgeWeightedIntGraph();
         Prim p = new Prim();
-        p.prim(g,0);
+        assertThrows(IllegalArgumentException.class, () -> p.prim(g, 0));
     }
 }
